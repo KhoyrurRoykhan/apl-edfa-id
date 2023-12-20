9d45cc49341c3230d5e50f21fa28c7b42f9ddc30
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-12-20 21:51:27 +0800

    review

--- src/components/UserReview.js
+++ src/components/UserReview.js
@@ -51,7 +51,7 @@ const UserReviews = () => {
       }
       .review-container {
         display: grid;
-        grid-template-columns: repeat(3, 1fr); 
+        grid-template-columns: repeat(5, 1fr); 
         gap: 20px;
         margin: 0 20px; 
         margin-bottom: 30px;
